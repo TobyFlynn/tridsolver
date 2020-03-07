7f39fbe39ad952e06678c7b57f42dd2d7adf5ffc
Toby Flynn <toby_flynn@btinternet.com>
2020-03-07 10:35:33 +0000

    Debug

--- scalar/src/cuda/trid_linear_mpi_reg8_double2.hpp
+++ scalar/src/cuda/trid_linear_mpi_reg8_double2.hpp
@@ -340,8 +340,27 @@ trid_linear_forward(const REAL *__restrict__ a, const REAL *__restrict__ b,
         store_array_reg8_double2<REAL>(cc,&l_cc,n, woffset, sys_size);
         store_array_reg8_double2<REAL>(aa,&l_aa,n, woffset, sys_size);
         
+        for(n = sys_size - (2*VEC); n > 0; n -= VEC) {
+          load_array_reg8_double2<REAL>(aa,&l_aa,n, woffset, sys_size);
+          load_array_reg8_double2<REAL>(cc,&l_cc,n, woffset, sys_size);
+          load_array_reg8_double2<REAL>(dd,&l_dd,n, woffset, sys_size);
+          
+          for(int i = VEC - 1; i >= 0; i--) {
+            d2 = l_dd.f[i] - l_cc.f[i] * d2;
+            a2 = l_aa.f[i] - l_cc.f[i] * a2;
+            c2 = -l_cc.f[i] * c2;
+            l_dd.f[i] = d2;
+            l_cc.f[i] = c2;
+            l_aa.f[i] = a2;
+          }
+          
+          store_array_reg8_double2<REAL>(dd,&l_dd,n, woffset, sys_size);
+          store_array_reg8_double2<REAL>(cc,&l_cc,n, woffset, sys_size);
+          store_array_reg8_double2<REAL>(aa,&l_aa,n, woffset, sys_size);
+        }
+        
         // Eliminate upper off-diagonal
-        for (int i = sys_size - VEC - 1; i > 0; --i) {
+        for (int i = VEC - 1; i > 0; --i) {
           int loc_ind = ind + i;
           dd[loc_ind] = dd[loc_ind] - cc[loc_ind] * dd[loc_ind + 1];
           aa[loc_ind] = aa[loc_ind] - cc[loc_ind] * aa[loc_ind + 1];
