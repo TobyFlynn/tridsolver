0c2294656e7a28f90e2dcf8e8b278af75c814f17
Toby Flynn <toby_flynn@btinternet.com>
2020-06-17 18:07:30 +0100

    Further pencil decomp code

--- scalar/src/cpu/trid_mpi_decomposition.hpp
+++ scalar/src/cpu/trid_mpi_decomposition.hpp
@@ -23,3 +23,44 @@ void subarray(MPI_Datatype datatype, int ndims, int sizes[ndims], int axis, int 
     MPI_Type_commit(&subarrays[p]);
   }
 }
+
+void exchange(MPI_Comm comm, MPI_Datatype datatype, int ndims, int sizesA[ndims], void *arrayA,
+              int axisA, int sizesB[ndims], void *arrayB, int axisB) {
+  int nparts;
+  MPI_Comm_size(comm, &nparts);
+  MPI_Datatype subarraysA[nparts], subarraysB[nparts];
+
+  subarray(datatype, ndims, sizesA, axisA, nparts, subarraysA);
+  subarray(datatype, ndims, sizesB, axisB, nparts, subarraysB);
+
+  int counts[nparts], displs[nparts];
+  for(int p = 0; p < nparts; p++) {
+    counts[p] = 1;
+    displs[p] = 0;
+  }
+
+  MPI_Alltoallw(arrayA, counts, displs, subarraysA,
+                arrayB, counts, displs, subarraysB, comm);
+
+  for(int p = 0; p < nparts; p++) {
+    MPI_Type_free(&subarraysA[p]);
+    MPI_Type_free(&subarraysB[p]);
+  }
+}
+
+void subcomm(MPI_Comm comm, int ndims, MPI_Comm subcomms[ndims]) {
+  MPI_Comm comm_cart;
+  int nprocs, dims[ndims], periods[ndims], remdims[ndims];
+  for(int i = 0; i < ndims; i++) {
+    dims[i] = periods[i] = remdims[i] = 0;
+  }
+  MPI_Comm_size(comm, &nprocs);
+  MPI_Dims_create(nprocs, ndims, dims);
+  MPI_Cart_create(comm, ndims, dims, periods, 1, &comm_cart);
+  for(int i = 0; i < ndims; i++) {
+    remdims[i] = 1;
+    MPI_Cart_sub(comm_cart, remdims, &subcomms[i]);
+    remdims[i] = 0;
+  }
+  MPI_Comm_free(&comm_cart);
+}
