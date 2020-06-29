6083fa3cd8e4be43b50f4b48e793544ca74b19c1
Toby Flynn <toby_flynn@btinternet.com>
2020-06-29 17:46:30 +0100

    Bug fix in MPI+CUDA test

--- scalar/test/test_cuda_mpi.cu
+++ scalar/test/test_cuda_mpi.cu
@@ -156,6 +156,7 @@ void test_solver_from_file(const std::string &file_name) {
     } else if(i == 1) {
       domain_offsets[i] = 0;
       local_sizes[i] = global_dim;
+      global_strides[i] = i == 0 ? 1 : global_strides[i - 1] * mesh.dims()[i - 1];
     } else {
       domain_offsets[i] = params.mpi_coords[i - 1] * (global_dim / mpi_dims[i - 1]);
       local_sizes[i] = params.mpi_coords[i - 1] == mpi_dims[i - 1] - 1
